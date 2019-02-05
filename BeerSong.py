6c7899aa942fc3179c48da1f50a7c7ce211d08ae
cyandura <35173272+cyandura@users.noreply.github.com>
2019-02-04 19:50:30 -0500

    added bottlesofbeer function because python doesnt need two functions

--- BeerSong.py
+++ BeerSong.py
@@ -1,4 +1,8 @@
 
 def bottlesofbeer():
-    
+    for x in range(98, -1, -1):
+        print("{} of beer on the wall, {} bottles of beer".format(x+1, x+1))
+        if x == 0: x = "no more"
+        print("take one down, pass it around, {} bottles of beer on the wall".format(x))
     return
+bottlesofbeer()
